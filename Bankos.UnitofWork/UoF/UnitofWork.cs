f6b4b36708885698c46f28800442f439c2561c34
Mina Nicola <minamahernicola@outlook.com>
2022-05-03 00:00:12 +0200

    added base of unit of work

---
+++ Bankos.UnitofWork/UoF/UnitofWork.cs
@@ -0,0 +1,29 @@
+﻿using Bankos.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankos.UnitofWork.UoF
+{
+    public class UnitofWork : IUnitofWork, IAsyncDisposable
+    {
+        private readonly MainContext _context;
+        public UnitofWork(MainContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
