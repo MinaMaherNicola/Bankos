bb75a3bcff9e0afbb0b8c373f9b7fedd63bf74e8
Mina Nicola <minamahernicola@outlook.com>
2022-05-02 23:51:46 +0200

    added basic base of the repository pattern

---
+++ Bankos.Repository/Repository/BankosRepository.cs
@@ -0,0 +1,32 @@
+﻿using Bankos.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankos.Repository.Repository
+{
+    public class BankosRepository<TEntity> : IBankosRepository<TEntity> where TEntity : class
+    {
+        private readonly MainContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public BankosRepository(MainContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+        public async Task<TEntity> Add(TEntity entity)
+        {
+            try
+            {
+                return (await _dbSet.AddAsync(entity)).Entity;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
